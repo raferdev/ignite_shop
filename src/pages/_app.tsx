2e6a08e136237a163da0856162c7ec1442cc9d6e
Rafael <rafinha_fern@hotmail.com>
2023-03-07 02:11:46 -0300

    feat: initial global css setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
+import { globalStyle } from '@/styles/global.js'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  globalStyle();
+  
   return <Component {...pageProps} />
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+import { globalCss } from "./index.js";
+
+export const globalStyle   = globalCss({
+'*':{
+    margin:0,
+    padding:0,
+},
+body:{
+    '-webkit-font-smoothing':'antialiased',
+},
+
+'body, input, textarea, button': {
+    fontFamily:'Roboto',
+    fontWeight:400,
+}
+
+})
