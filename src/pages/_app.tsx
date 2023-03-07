39ce823e9c91b601192432cfb19c7f5466dadf2c
Rafael <rafinha_fern@hotmail.com>
2023-03-07 02:39:19 -0300

    feat: add styled components on app: header and container wrapper.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,19 @@
 import { globalStyle } from '@/styles/global.js'
+import { Container, Header } from '@/styles/pages/app.js';
 import type { AppProps } from 'next/app'
-
+import Image from 'next/image.js';
+import logoImg from '../assets/logo.svg';
 globalStyle();
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  return <Component {...pageProps} />
+  return( 
+  <Container>
+    <Header>
+      <Image src={logoImg} alt=''/>
+    </Header>
+    <Component {...pageProps} />
+  </Container>
+  
+  )
 }
