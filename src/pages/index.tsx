c0b8e1cd71293f1dc87a54e3ee2ff716b5580076
Rafael <rafinha_fern@hotmail.com>
2023-03-08 00:05:35 -0300

    feat: new home page with components and styles.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { HomeContainer, Product } from "@/styles/pages/home.js";
+import Image from "next/image.js";
+
 export default function Home() {
   return (
-<h1>hello world</h1>
+<HomeContainer>
+  <Product>
+    <Image src={''} width={520} height={480} alt={''}/>
+  </Product>
+</HomeContainer>
   )
 }
