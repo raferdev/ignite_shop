fcef9b6abe451c59776bc511ba54353278bd82db
Rafael <rafinha_fern@hotmail.com>
2023-03-16 14:36:17 -0300

    fix: home styles.

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -5,14 +5,14 @@ export const HomeContainer = styled('main',{
     display:'flex',
     backgroundColor:'transparent',
     width:'100%',
-    maxWidth:'calc(100vw - ((100vw-1180px)/2))',
+    maxWidth:'calc(100vw - ((100vw - 1180px) / 2))',
     marginLeft:'auto',
     minHeight: 656,
 
 });
 
 export const Product = styled('a', {
-    background: 'linear-gradient(180deg, #ea483 0%, #7465d4 100%)',
+    background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
     borderRadius: 8,
     padding: '0.25rem',
     cursor:'pointer',
