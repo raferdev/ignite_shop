4814287bd00975332969cb5c14e06f26f778ef10
Rafael <rafinha_fern@hotmail.com>
2023-03-16 14:53:17 -0300

    feat: add new keen-slide on products view.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,11 @@ import { GetServerSideProps } from "next";
 import Image from "next/image";
 import Stripe from "stripe";
 
+import React from 'react'
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react' // import from 'keen-slider/react.es' for to get an ES module
+
+
 interface HomeProps {
   products:{
 
@@ -15,11 +20,18 @@ interface HomeProps {
 
 }
 export default function Home({products}:HomeProps) {
-  return (
-<HomeContainer>
+ 
+ const [sliderRef] =  useKeenSlider({
+  slides:{
+    perView:3,
+    spacing:48
+ }})
+
+ return (
+<HomeContainer ref={sliderRef} className="keen-slider">
 {products.map(product => {
   return (
-  <Product key={product.id}>
+  <Product key={product.id} className="keen-slider__slide">
     <Image src={product.imageUrl} width={520} height={480} alt={''}/>
   <footer>
     <strong>
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -7,14 +7,13 @@ export const HomeContainer = styled('main',{
     width:'100%',
     maxWidth:'calc(100vw - ((100vw - 1180px) / 2))',
     marginLeft:'auto',
-    minHeight: 656,
+    minHeight: 456,
 
 });
 
 export const Product = styled('a', {
     background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
     borderRadius: 8,
-    padding: '0.25rem',
     cursor:'pointer',
     position:'relative',
 
