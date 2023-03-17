d3cfa6d04bf3415491417f1817d11acf35111c05
Rafael <rafinha_fern@hotmail.com>
2023-03-17 12:55:46 -0300

    ffix: change prefetching to false.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home({products}:HomeProps) {
 {products.map(product => {
   return (
     <Link 
-    href={`/product/${product.id}`} key={product.id}>
+    href={`/product/${product.id}`} key={product.id} prefetch={false}>
   <Product
   className="keen-slider__slide">
 
